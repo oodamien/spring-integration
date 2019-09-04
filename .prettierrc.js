6d8f9b059ef0ab97626c42fc546c6caf85f91da8
Damien Vitrac <dvitrac@pivotal.io>
2019-09-04 11:01:13 +0200

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  semi: false,
+  printWidth: 80,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
